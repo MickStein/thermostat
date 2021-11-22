13e4595cbd5b953da15d996691c4bac1acf11af0
michael steinacher <steinacherm@hotmail.co.uk>
2021-11-22 09:33:15 +0000

    Added the increase and decreasing temp functions to the interface which increases/decreases the temp text

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+// This top level ensures the functions within do not run until the page it loaded
 document.addEventListener("DOMContentLoaded", () => {
   const thermostat = new Thermostat();
   document.querySelector('#temp').innerText = thermostat.temp
-})
+
+  document.querySelector('#temp-up').addEventListener('click', () => {
+    thermostat.up(); 
+    document.querySelector('#temp').innerText = thermostat.temp;
+  });
+  
+  document.querySelector('#temp-down').addEventListener('click', () => {
+    thermostat.down();
+    document.querySelector('#temp').innerText = thermostat.temp;
+  });  
+});
+
